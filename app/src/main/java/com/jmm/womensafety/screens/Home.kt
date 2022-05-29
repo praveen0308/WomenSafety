a68696627d8282cb78c3c0380dce1b768c6f5e22
Praveen Yadav <yadavpravin0308@gmail.com>
2022-05-30 03:09:48 +0530

    final commit

--- app/src/main/java/com/jmm/womensafety/screens/Home.kt
+++ app/src/main/java/com/jmm/womensafety/screens/Home.kt
@@ -1,18 +1,25 @@
 package com.jmm.womensafety.screens
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.jmm.womensafety.databinding.FragmentHomeBinding
 import com.jmm.womensafety.util.BaseFragment
 import java.util.*
 
 
 class Home : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
-
+    private lateinit var dialog: ProgressDialog
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        dialog = ProgressDialog(requireContext())
+        dialog.setTitle("Sending Location")
+        dialog.setMessage("Please wait...")
+        dialog.isIndeterminate = true
+
         binding.apply {
             cardNearByPoliceStation.setOnClickListener {
                 val map = "http://maps.google.co.in/maps?q=police station near me"
@@ -20,7 +27,25 @@ class Home : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
                 startActivity(intent)
             }
             cardHarassment.setOnClickListener {  }
-            cardSendLocation.setOnClickListener {  }
+            cardSendLocation.setOnClickListener {
+                dialog.show()
+                val t = Timer()
+                t.schedule(object : TimerTask() {
+                    override fun run() {
+                        dialog.dismiss() // when the task active then close the dialog
+                        activity!!.runOnUiThread {
+                            Toast.makeText(
+                                activity,
+                                "Sent successfully !!!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                        t.cancel() // also just top the timer thread, otherwise, you may receive a crash report
+
+                    }
+                }, 3000)
+
+            }
         }
     }
 
